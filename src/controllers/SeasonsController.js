a4a760a8014d646c277b0437108c4c896a40b5c3
David Padilla <davidpaag2002@gmail.com>
2024-03-28 16:45:49 +0100

    feat(addSeason): Working first Season endpoint

--- src/controllers/SeasonsController.js
+++ src/controllers/SeasonsController.js
@@ -1,20 +1,20 @@
 import { db } from '#Root/firebase.js';
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 
 class SeasonsController {
 
     async addSeason(req, res) {
         console.log(req.body);
-        const { seasonCode, seasonLiters } = req.body;
+        const { seasonCode } = req.body;
         const seasonRef = db.collection("Seasons").doc(uuid());
         const res2 = await seasonRef.set({
             "seasonCode": seasonCode,
-            "seasonLiters": seasonLiters
+            "seasonLiters": 0
         })
 
-        res.status(200).send("Correcto")
+        res.status(200).send("Correcto");
     }
 }
 
 
-export default SeasonsController;
+export default new SeasonsController;
--- src/routes/seasons.routes.js
+++ src/routes/seasons.routes.js
@@ -3,8 +3,58 @@ import { Router  } from "express";
 import SeasonsController from "#Controllers/SeasonsController.js";
 
 const seasonsRouter = Router();
+// var seasonsController = new SeasonsController();
 
-
-seasonsRouter.post("/addSeason", SeasonsController);
+/**
+ * @openapi
+ * /seasons/addSeason:
+ *   post:
+ *     summary: Add a new agricultural season
+ *     tags:
+ *       - Seasons
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/AddSeasonSchema"
+ *     responses:
+ *       200:
+ *         description: Added a new Season!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *       5XX:
+ *         description: Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/ErrorsSchema"
+ * components:
+ *   schemas:
+ *     AddSeasonSchema:
+ *       type: object
+ *       properties:
+ *         seasonCode: 
+ *           type: string
+ *           example: 23/24 
+ *       required:
+ *         - seasonCode
+ *  
+ *     ErrorsSchema:
+ *       type: object
+ *       properties:
+ *         errors: 
+ *           type: array
+ *           items: 
+ *             type: string
+ *             example: Must have required property 'seasonCode'
+ */
+seasonsRouter.post("/addSeason", SeasonsController.addSeason);
 
 export default seasonsRouter;
