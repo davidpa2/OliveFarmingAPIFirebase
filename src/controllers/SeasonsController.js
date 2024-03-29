437bf74b4f1f426eb035c7731e91a15fa1bb0e85
David Padilla <davidpaag2002@gmail.com>
2024-03-29 16:56:51 +0100

    fix(addSeason): Fixed addSeason endpoint

--- src/controllers/SeasonsController.js
+++ src/controllers/SeasonsController.js
@@ -1,18 +1,29 @@
 import db from '#Root/firebase.js';
+import { collection, query, where, getDocs, doc, setDoc } from 'firebase/firestore';
 import { v4 as uuid } from 'uuid'
 
 class SeasonsController {
 
     async addSeason(req, res) {
-        console.log(req.body);
         const { seasonCode } = req.body;
-        const seasonRef = db.collection("Seasons").doc(uuid());
-        const res2 = await seasonRef.set({
-            "seasonCode": seasonCode,
-            "seasonLiters": 0
-        })
+        const seasonRef = collection(db, "Seasons");
 
-        res.status(200).send("Correcto");
+        // Check if there is a season with this seasonCode
+        const q = query(seasonRef, where("seasonCode", "==", seasonCode));
+        var season = null;
+        // Get the results
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            season = doc;
+        });
+        if (season) return res.status(409).send({ errors: ['Ya existe una temporada de lluvia con ese código'] });
+
+        // Upload new Doc
+        await setDoc(doc(db, "Seasons", uuid()), {
+            "seasonCode": seasonCode
+        });
+
+        res.status(200).send("Se ha introducido una nueva temporara agrícola");
     }
 }
 
